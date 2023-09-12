0079bb783b76164736bca4b2cfa84b2fdfa84c18
jueunyang08 <sa04129@naver.com>
2023-09-13 01:50:56 +0900

    0913

---
+++ php/home/230912.php
@@ -0,0 +1,39 @@
+<?php 
+echo "hello world!\n";
+
+$age = "20";
+$name ="jueun";
+
+echo "hello, {$name}! 당신은 {$age}살 입니다.\n";
+
+// 숫자를 입력받아 10까지 해당숫자의 구구단을 표시하는 스크립트를 생성합니다.
+// function num($i) {
+// $total = 1;
+// for($i=1;$i<=10;$i++) {
+// $total*$i;
+
+// }
+// return $i;
+// }
+// echo num(1);
+
+//-------------------------------
+// function ex($num) {
+// for($i=1;$i<=10;$i++)
+// {    
+//     $total = $num*$i;
+//     echo "{$num} * {$i} = {$total}\n";}
+
+//     return $num;
+
+// }
+
+// echo ex(3);
+//---------------------------
+function m($arr) {
+    foreach ($arr as $val) {
+       
+    }
+    return $arr;
+}
+echo max(5,4,85,81,2,);

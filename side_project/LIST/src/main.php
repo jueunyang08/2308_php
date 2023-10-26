8a395dadfb39102cf557122a5aacc44e85885a73
jueunyang08 <sa04129@naver.com>
2023-10-26 19:47:17 +0900

    231026list

--- side_project/LIST/src/main.php
+++ side_project/LIST/src/main.php
@@ -3,15 +3,24 @@
 xcopy D:\workspace\2308_php\side_project\LIST C:\Apache24\htdocs\LIST /E /Y
 -->
 
+<?php
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/LIST/src/"); //웹서버
+require_once(ROOT. "lib/lib_db.php"); // DB 관련 라이브러리
+?>
+
 <!DOCTYPE html>
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>list</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/LIST/src/css/list.css">
 </head>
 <body>
-    
+    <main class="bg">
+<div class ="main_div">
+    ddfdfdfdfdfd
+</div>
+    </main>
 </body>
 </html>

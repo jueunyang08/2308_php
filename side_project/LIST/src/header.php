925e5b2eaace6a1e09193e74fc2d839bb2e16bac
jueunyang08 <sa04129@naver.com>
2023-10-28 15:49:25 +0900

    list

---
+++ side_project/LIST/src/header.php
@@ -0,0 +1,5 @@
+<header>
+<div class='main_header'>
+            <button class='btn_insert cursor'><a class="plus">+</a></button>
+        </div>
+</header>
--- side_project/LIST/src/main.php
+++ side_project/LIST/src/main.php
@@ -5,7 +5,9 @@ xcopy D:\workspace\2308_php\side_project\LIST C:\Apache24\htdocs\LIST /E /Y
 
 <?php
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/LIST/src/"); //웹서버
+define("FILE_HEADER", ROOT."header.php"); // 헤더 패스
 require_once(ROOT. "lib/lib_db.php"); // DB 관련 라이브러리
+
 ?>
 
 <!DOCTYPE html>
@@ -19,9 +21,31 @@ require_once(ROOT. "lib/lib_db.php"); // DB 관련 라이브러리
 <body>
 <main>
     <div class= 'main_div center'>
-        <div class='main_header'>
-            <button class='btn_insert'>+</button>
+        <!-- 헤더파일 -->
+        <?php
+        require_once(FILE_HEADER);
+        ?>
+    <p class = 'TODOLIST'>TO DO LIST</p>
+
+    <div class= 'main_page_list_section'>
+        <div class='align'>
+            <ul>
+                <li><div class = 'bullet'><p class='num'>3</p></div><span class='list_name'>To do gggggggggggggggggggggggggggggggggggggggggggggglisfeeft 3</span>
+                <div class="border_line"></div>
+                </li>
+                <li><div class = 'bullet'><p class='num'>3</p></div><span class='list_name'>To do list 3</span>
+                <div class="border_line"></div>
+                </li>
+                <li><div class = 'bullet'><p class='num'>3</p></div><span class='list_name'>To do list 3</span>
+                <div class="border_line"></div>
+                </li>
+            </ul>
         </div>
+    </div>
+
+    <div class='page_footer_section'>
+        <button class='prev_btn footer_position'><a href=""></a></button>
+        <button class='next_btn footer_position'><a href=""></a></button>
     </div>
 </main>
 </body>

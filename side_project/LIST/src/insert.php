d27f947896e0b2d8c17edc4433bd9d823b18f1ef
jueunyang08 <sa04129@naver.com>
2023-10-30 18:11:45 +0900

    1030 리스트

--- side_project/LIST/src/insert.php
+++ side_project/LIST/src/insert.php
@@ -21,17 +21,22 @@ require_once(ROOT. "lib/lib_db.php"); // DB 관련 라이브러리
             require_once(FILE_HEADER);
             ?>
             <!-- 컨텐츠 영역 -->
-            <div class= 'content_section'>
+            <form action="">
+                <div class= 'content_section'>
+                    <div class='insert_title'>
+                        <input class='insert_title w_title' type="text" maxlength='16'><p class = 'w_title'>제목이길어요제목이제목이길어요제</p>
+                    </div>
 
-            </div>
-        
-            <!-- 페이지 푸터 영역 -->
-            <div class='page_footer_section'>
-                <!-- 취소 버튼 -->
-                <button class='footer_position c_w_btn cursor' onclick="location.href='/LIST/src/main.php'">취소</button>
-                <!-- 작성 버튼 -->
-                <button class='footer_position c_w_btn w_btn cursor'>작성</button>
-            </div>
+                </div>
+            
+                <!-- 페이지 푸터 영역 -->
+                <div class='page_footer_section'>
+                    <!-- 취소 버튼 -->
+                    <button class='footer_position c_w_btn cursor' onclick="location.href='/LIST/src/main.php'">취소</button>
+                    <!-- 작성 버튼 -->
+                    <button class='footer_position c_w_btn w_btn cursor'>작성</button>
+                </div>
+            </form>
         </div>
     </main>
 </body>

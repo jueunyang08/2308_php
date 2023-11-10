4764f6887660bfe5e527730034428010f2f76451
jueunyang08 <sa04129@naver.com>
2023-11-10 17:07:53 +0900

    1110 라라벨

--- laravel_edu/routes/web.php
+++ laravel_edu/routes/web.php
@@ -15,6 +15,21 @@ use Illuminate\Http\Request;
 |
 */
 
+// {{--
+        
+//     server 시작 하는 방법
+//     터미널 / php artisan serve
+    
+//     server 종료 하는 방법
+//     ctrl + C
+
+//     서버 열고, c> Apache24> htdocs 로 파일 옮겨주기
+    
+    
+// --}}
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -40,7 +55,7 @@ Route::get('/myview', function () {
 // 여러 라우터에 해당될 경우 가장 마지막에 정의 된것이 실행
 // ----------------------------------------------------
 
-
+// get, post, put, delete
 // get method로 localhost/home으로 접속했을 때 'home' 뷰 파일을 리턴
 Route::get('/home', function () {
     return view('home');
@@ -104,5 +119,18 @@ Route::get('/name/home/php504/user', function () {
     return '이름있는 라우트';
 })->name('name.user');
 
-// get, post, put, delete
+// --------
+// 컨트롤러
+// --------
+
+// 커멘드로 컨트롤러 생성 :
+
+// php artisan make:controller 컨트롤러명
+use App\Http\Controllers\TestController;
+Route::get('/test', [TestController::class, 'index'])->name('test.index');
+
+// php artisan make:controller 컨트롤러명 --resource
+use App\Http\Controllers\TaskController;
+Route::resource('/task', TaskController::class);
+
 

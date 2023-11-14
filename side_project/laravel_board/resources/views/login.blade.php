e76719cd4b13a3a08b8b7fb7d311a71b20332f06
jueunyang08 <sa04129@naver.com>
2023-11-14 18:08:38 +0900

    1114 수업

--- side_project/laravel_board/resources/views/login.blade.php
+++ side_project/laravel_board/resources/views/login.blade.php
@@ -5,5 +5,10 @@
 @section('main')
     <main>
         <h1>Login</h1>
+        {{-- <label for="id">ID</label>
+        <input type="text" id="id" name="id">
+        <br>
+        <label for="pw">PW</label>
+        <input type="password" id="pw" name="pw"> --}}
     </main>
 @endsection
